c32addf81af3bab41b7e2c775efe0f6a7251aeb6
Pavankumar Shivashetty <pavankumar_shivashetty@epam.com>
2022-01-06 00:02:17 +0530

    Added eureka client to pom.xml and added app name to properties file
    and some minor changes

--- UserServiceApplication/src/main/java/com/epam/UserServiceApplication.java
+++ UserServiceApplication/src/main/java/com/epam/UserServiceApplication.java
@@ -3,9 +3,11 @@ package com.epam;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
